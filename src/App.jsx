85c7d187b1cdbc7ccd3f0f025f3b4309a0ba7177
SAISHANMUKHASANGU <sangusaishanmukha@gmail.com>
2024-11-29 18:25:33 +0530

    first add

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './components/layout/footer'
+import Header from './components/layout/header'
+import Main from './components/layout/main'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/> 
+    <Main />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,18 @@
+
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footerdata">2024 All Rights Reserved</p>
+        <nav className="navigation">
+                    <a id="navele" href="/html/">Facebook</a>
+                    <a id="navele" href="/css/">Instagram</a> 
+                    <a id="navele" href="/js/">Twitter</a>
+        </nav>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
